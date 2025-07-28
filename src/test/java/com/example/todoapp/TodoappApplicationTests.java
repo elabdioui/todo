e74e98187d88e4213600343a73c347b623fb4ace
elabdioui <elabdiouihaitam@gmail.com>
2025-07-28 11:52:29 +0100

    second commit

--- src/test/java/com/example/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/example/todoapp/TodoappApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.todoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Active le profil test
 class TodoappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Ce test vérifie simplement que le contexte Spring se charge correctement
 	}
 
 }
