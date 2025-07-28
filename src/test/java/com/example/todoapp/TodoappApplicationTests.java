1c6f9719c5faa9f6baf764fa690904a0251dc0c8
elabdioui <elabdiouihaitam@gmail.com>
2025-07-28 13:09:25 +0100

    adding application-test.properties

--- src/test/java/com/example/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/example/todoapp/TodoappApplicationTests.java
@@ -1,16 +1,20 @@
 package com.example.todoapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@ActiveProfiles("test") // Active le profil test
+@TestPropertySource(locations = "classpath:application-test.properties")
 class TodoappApplicationTests {
+
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
 
 	@Test
 	void contextLoads() {
-		// Ce test vérifie simplement que le contexte Spring se charge correctement
+		System.out.println("Tests running with database: " + datasourceUrl);
+		// Cela vous montrera quelle configuration est utilisée
 	}
-
 }
